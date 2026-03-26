3562965a320a67b05a8b24539828da482e8da2f1
Jonte Craighead <jontec@users.noreply.github.com>
2026-03-25 21:32:31 -0700

    Add pipeline lambdas to enable direct execution of tasks from workbench cli

--- lib/workbench/cli.rb
+++ lib/workbench/cli.rb
@@ -2,10 +2,10 @@ require "thor"
 
 module Workbench
   class CLI < Thor
-    desc "start", "start <pipeline> or start /path/to/pipeline[.y*ml] Run the specified pipeline using tasks from ./tasks"
+    desc "start", "start <pipeline> or start /path/to/pipeline[.y*ml] Run the specified pipeline or task"
     option :verbose, type: :boolean
     option :input, type: :hash, desc: "Input values for the pipeline"
-    def start(pipeline_or_path_to_file)
+    def start(pipeline_name_or_path_or_task)
       # default path will be pipelines/ directory from the project root
       # if this were built in rails, we'd always be able to find the application root directory
       # should we have a workbench config file to flag where the approot should be?
@@ -14,7 +14,10 @@ module Workbench
       # when we need to look up tasks we can just load all of them from memory (if we've loaded them already)
       puts "Verbose mode ON" if options[:verbose]
       Task.task_dir = options[:task_dir] if options[:task_dir]
-      pipeline = Pipeline.find_by_name_or_path(pipeline_or_path_to_file)
+      pipeline = Pipeline.find_by_name_or_path(pipeline_name_or_path_or_task)
+      unless pipeline
+        pipeline = Pipeline.lambda([pipeline_name_or_path_or_task])
+      end
       if options[:input]
         pipeline.context.merge!(options[:input].deep_symbolize_keys)
       end
